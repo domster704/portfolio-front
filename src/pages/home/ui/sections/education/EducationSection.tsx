6fe0a5be00823d315960f75eab5a09d9546a8e4a
domster704 <domster704@mail.ru>
2026-08-24 22:32:03 +0300

    add education section

--- src/pages/home/ui/sections/education/EducationSection.tsx
+++ src/pages/home/ui/sections/education/EducationSection.tsx
@@ -1,8 +1,17 @@
 import { type FC } from "react";
 import style from "./EducationSection.module.scss";
+import {useTranslations} from "next-intl";
+import Section from "@shared/ui/section";
+import {SECTIONS} from "@shared/constants/sections";
 
 const EducationSection: FC = ({}) => {
-  return <div className={style.content}>content</div>;
+  const t = useTranslations("Education");
+
+  return (
+    <Section id={SECTIONS.EDUCATION} title={t("title")} label={t("label")}>
+      <></>
+    </Section>
+  );
 };
 
 export default EducationSection;
