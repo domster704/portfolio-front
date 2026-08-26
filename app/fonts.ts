311a50da54e68e7c363ddda055355b4eac6963b1
domster704 <domster704@mail.ru>
2026-08-26 11:38:49 +0300

    optimize fonts

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,7 +1,7 @@
 import localFont from "next/font/local";
 
 export const inter = localFont({
-  src: "./fonts/Inter-VariableFont.ttf",
+  src: "./fonts/Inter-VariableFont-ru-en.woff2",
   variable: "--font-inter",
   display: "swap",
 });
